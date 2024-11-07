6c4b98eb7fad56d2a1702f96331acc061afcd29f
KamogeloS94 <kamogeloseepamere@gmail.com>
2024-11-08 00:02:07 +0200

    spring-cloud-microservices

---
+++ spring-cloud-microservices/infrastructure/config-service/ConfigServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
---
+++ spring-cloud-microservices/infrastructure/config-service/src/main/resources/application.properties/EurekaServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
